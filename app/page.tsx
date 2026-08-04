5a0d9b91e104a7e738f4d870e71f65474906caca
TVupele <tvupele@gmail.com>
2026-08-04 17:30:02 +0100

    Fixed logo size

--- app/page.tsx
+++ app/page.tsx
@@ -112,8 +112,8 @@ export default function Home() {
           <Image
             src="/patterns/creative-collective-logo.png"
             alt="Creative Collective Africa"
-            width={2108}
-            height={1841}
+            width={1908}
+            height={1641}
             className="mx-auto h-auto w-72 sm:w-80"
           />
 
